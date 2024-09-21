24eb2847820c24b7f0b6b79a4d6e662261f99f57
deji100 <adeyemideji9@gmail.com>
2024-09-21 17:09:57 +0100

    updated

--- app/components-v2/links.js
+++ app/components-v2/links.js
@@ -5,11 +5,11 @@ import { Link } from "react-scroll";
 const Links = () => {
     return (
         <div className="scroll-link">
-            <Link to="about" spy={true} smooth={true} duration={500} className="link">About</Link>
-            <Link to="cdm" spy={true} smooth={true} duration={500} className="link">CDM</Link>
-            <Link to="solution" spy={true} smooth={true} duration={500} className="link">Solution Suite</Link>
-            <Link to="partner" spy={true} smooth={true} duration={500} className="link">Partners</Link>
-            <Link to="faq" spy={true} smooth={true} duration={500} className="link">FAQ</Link>
+            <Link to="about" spy={true} smooth={true} duration={500} offset={-100} className="link">About</Link>
+            <Link to="cdm" spy={true} smooth={true} duration={500} offset={-100} className="link">CDM</Link>
+            <Link to="solution" spy={true} smooth={true} duration={500} offset={-100} className="link">Solution Suite</Link>
+            <Link to="partner" spy={true} smooth={true} duration={500} offset={-200} className="link">Partners</Link>
+            <Link to="faq" spy={true} smooth={true} duration={500} offset={-100} className="link">FAQ</Link>
         </div>
     )
 }
